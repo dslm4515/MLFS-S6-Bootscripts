cfc21067a0016d4070e6afc2ae32767b70a09b4c
dslm4515 <mlfs.dev.@gmail.com>
2021-01-14 02:34:57 -0600

    Added script for re-initializing /etc/s6/base if ever it is copied or unpacked from an archive.

---
+++ init-s6-base.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Re-initializing init base..."
+rm -rf /etc/s6/base &&
+s6-linux-init-maker -1 -f /etc/s6/skel -p "/bin:/sbin:/usr/bin"    \
+	            -D default -G "/sbin/agetty -L -8 tty1 115200" \
+		    -c /etc/s6/base -t 2 -L -u root -U utmp /etc/s6/base
+rm -rf /etc/s6/base/scripts
+cp -r /etc/s6/scripts /etc/s6/base/scripts
