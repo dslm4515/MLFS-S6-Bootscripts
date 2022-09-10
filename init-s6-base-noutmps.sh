8c65c1a49026eafca8cd2cc94a20b1e0ccdb05e4
dslm4515 <27793627+dslm4515@users.noreply.github.com>
2022-09-10 07:43:38 -0500

    Added scripts for systems with or without skarnet's utmps

--- init-s6-base-noutmps.sh
+++ init-s6-base-noutmps.sh
@@ -4,4 +4,4 @@ echo "Re-initializing init base..."
 rm -rf /etc/s6/base &&
 s6-linux-init-maker -1 -f /etc/s6-linux-init/skel -p "/bin:/sbin:/usr/bin:/usr/sbin"    \
 	            -D default -G "/sbin/agetty -L -8 tty1 115200" \
-		    -c /etc/s6/base -t 2 -L -u root -U utmp /etc/s6/base
+		    -c /etc/s6/base -t 2 -L -u root /etc/s6/base
