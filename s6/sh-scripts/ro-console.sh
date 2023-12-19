3dfecf31e8e7ea160f16e8bb9afed0a872779d2f
dslm4515 <27793627+dslm4515@users.noreply.gitub.com>
2023-12-19 10:36:55 -0600

    Console font can now be set in s6.conf
    This removes the need to install the terminus console font

--- s6/sh-scripts/ro-console.sh
+++ s6/sh-scripts/ro-console.sh
@@ -3,13 +3,23 @@
 . /etc/s6/sh-scripts/common_funcs.sh
 . /etc/s6/s6.conf
 
-GETTYS="$GETTYS tty2 tty1"
+ETTYS="$GETTYS tty2 tty1"
 
-msg "Set font & unicode mode for $GETTYS ...\n"
+msg "Set font & unicode mode for $ETTYS ...\n"
 
-for t in $GETTYS
+# Enable unicode mode
+for t in $ETTYS
 do
 	unicode_start   < /dev/$t
-	setfont $CFONT -C /dev/$t
 done
+
+# Set console font
+if [ $CFONT != "0" ]; then
+	for t in $ETTYS
+	do
+		setfont $CFONT -C /dev/$t
+	done
+else
+	msg "Using default console font.\n"
+fi
 msg_ok
